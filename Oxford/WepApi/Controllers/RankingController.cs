df6326cb09791d3806ad67278d5df4b301cebdb4
Slava Agafonov <Agafonov.Slava@hotmail.com>
2018-05-05 09:53:43 -0700

    Add Files and ranking controller

---
+++ Oxford/WepApi/Controllers/RankingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Oxford.CSHttpClientSample;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class RankingController : Controller
+    {
+        [HttpGet]
+        public string GetRanking(string keywords)
+        {
+            if (string.IsNullOrWhiteSpace(keywords)) return "keywords cannot be empty";
+            string result = OcrProgram.MakeAnalysisRequest(keywords);
+            if (result == "Bad Request") throw new ApplicationException("Bad request");
+
+            List<string> resultWords = OcrProgram.ExtractWords(result);
+            string fullText = OcrProgram.DisplayWords(resultWords);
+            if(string.IsNullOrWhiteSpace(fullText)) throw new ApplicationException("No text on image was recognized");
+            return fullText;
+        }
+    }
+}
